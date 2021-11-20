d34204aca0a0ae87645715a910b06dedaceebdbc
aradaki <takukanaspy@gmail.com>
2021-11-20 08:31:57 +0000

    defshow

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,12 +1,4 @@
 class Customers::CustomersController < ApplicationController
   def show
-    @customer = current_customer
-  end
-
-  def edit
-    @customer = current_customer
-  end
-  def withdraw
-    @customer = current_customer
   end
 end
