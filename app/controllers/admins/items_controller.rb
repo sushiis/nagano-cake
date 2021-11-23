697763e4c72118973f1c4446d101f8e8ea7e8e7b
yaoki87 <878787yao@gmail.com>
2021-11-23 02:52:29 +0000

    新規投稿機能途中

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,5 +1,24 @@
 class Admins::ItemsController < ApplicationController
+
   def index
     @items = Item.all
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
+    @items.genre_id = Item.new(item_params)
+    @items.save
+    redirect_to admins_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :status, :genre_id)
+  end
+
 end
