65f0eee78ee1b8a956e068ee7f06c9453d5a1c29
aradaki <takukanaspy@gmail.com>
2021-11-23 13:48:54 +0000

    new退会機能

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,12 +1,18 @@
 class Customers::CustomersController < ApplicationController
   def show
   end
-  
+
   def withdraw
     @customer = current_customer
     @customer.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理が完了しました。"
     redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :farst_name, :last_name_kana, :farst_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 end
