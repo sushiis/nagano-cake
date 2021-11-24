48588e4aa71a73c71f1bab4f3efcbf5a92b6fc2f
aradaki <takukanaspy@gmail.com>
2021-11-24 14:21:12 +0000

    配送機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.create!(
+Admin.create!(
     email: 'admin@admin.com',
     password: 'admintest'
   )
