9024d9f4d444d15c02454aad3e39d9b28644cc0b
yoshikawakoki <cooler970708@gmail.com>
2021-11-27 04:01:44 +0000

    修正

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -2,15 +2,14 @@ class Customers::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @orders = current_customer.order
-    @items = Order.items
+    @orders = current_customer.orders
   end
 
   def show
     @order = Order.find(params[:id])
     @order_details = Order.order_details
   end
-  
+
   def new
     @order = Order.new
     @customer = current_customer
